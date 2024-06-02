bdd68544e35ba2701ea7732822d41e10521c3c13
Petech <petekshamitbeyli@gmail.com>
2024-06-02 23:43:58 +0300

    navbar-shoppingbag

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { ShoppingBag } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -54,6 +56,17 @@ export default function Navbar() {
             </div>
           ))}
         </nav>
+        <div className="flex divide-x border-r sm:border-l">
+          <Button
+            variant={"outline"}
+            className="flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none"
+          >
+            <ShoppingBag />
+            <span className="hidden text-xs font-semibold text-gray-500 sm:block">
+              Cart
+            </span>
+          </Button>
+        </div>
       </div>
     </header>
   );
