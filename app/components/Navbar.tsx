094f409357bd78fdcefb8cd5e753a0fe95be42dc
Petech <petekshamitbeyli@gmail.com>
2024-06-02 23:30:27 +0300

    navbar-start

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const links = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "Men",
+    href: "/Men",
+  },
+  {
+    name: "Women",
+    href: "/Women",
+  },
+  {
+    name: "Teens",
+    href: "/Teens",
+  },
+];
+
+export default function Navbar() {
+  const pathname = usePathname();
+
+  return (
+    <header className="mb-8 border-b">
+      <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
+        <Link href="/">
+          <h1 className="text-4xl font-bold">
+            Next <span className="text-primary">Commerce</span>
+          </h1>
+        </Link>
+        <nav className="hidden gap-12 lg:flex 2xl:ml-16">
+          {links.map((link, idx) => (
+            <div key={idx}>
+              {pathname === link.href ? (
+                <Link
+                  className="text-lg font-semibold text-primary"
+                  href={link.href}
+                >
+                  {link.name}
+                </Link>
+              ) : (
+                <Link
+                  href={link.href}
+                  className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-primary"
+                >
+                  {link.name}
+                </Link>
+              )}
+            </div>
+          ))}
+        </nav>
+      </div>
+    </header>
+  );
+}
