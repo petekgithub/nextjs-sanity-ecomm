15b6344e8a6cb42a0e7cfea68a744c5f20a922c5
Petech <petekshamitbeyli@gmail.com>
2024-06-03 00:34:16 +0300

    hero-section-text

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
 import Image from "next/image";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello from the main page</h1>
-    </div>
+    <section className="mx-auto max-w-2xl px-4 sm:pb-6 lg:max-w-7xl lg:px-8">
+      <div className="mb-8 flex-wrap justify-between md:mb-16">
+        <div className="mb-6 flex w-full flex-col justify-center sm:mb-12 lg:mb-0 lg:w-1/3 lg:pb-24 lg:pt-48">
+          <h1 className="mb-4 text-4xl font-bold text-black sm:text-5xl md:mb-8 md:text-6xl">
+            Top Fashion for a top price !
+          </h1>
+          <p className="max-w-md leading-relaxed text-gray-500 xl:text-lg">
+            We sell only the most exclusive and high quality products for you.
+            We are the best so come and shop with us.
+          </p>
+        </div>
+      </div>
+    </section>
   );
 }
